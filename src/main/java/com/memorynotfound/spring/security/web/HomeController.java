06b0928f03f3685e19f3966aa50465fe07985e00
Roman Movcheniuk <roman.movcheniuk@motorolasolutions.com>
2020-05-24 15:47:33 +0300

    Secure login

---
+++ src/main/java/com/memorynotfound/spring/security/web/HomeController.java
@@ -0,0 +1,34 @@
+package com.memorynotfound.spring.security.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
+    }
+
+    @GetMapping("/protected")
+    public String info() {return "/error/successful_login";}
+
+    @GetMapping("/unprotected")
+    public String test() {return "/error/successful_login";}
+}
